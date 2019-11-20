fa9f39cabd455f350cc7207e1129d7aa70d142f5
Emily <emily.selwood@sa.catapult.org.uk>
2019-11-20 11:37:06 +0000

    path.split doesn't do what I thought

--- utils/prep_utils.py
+++ utils/prep_utils.py
@@ -113,6 +113,27 @@ def get_url(url, user=None, password=None):
         return response
 
 
+def split_all(path):
+    """
+    split_all takes a path and splits it into a list of directories and files.
+    :param path: path to be split
+    :return: a list of parts.
+    """
+    all_parts = []
+    while 1:
+        parts = os.path.split(path)
+        if parts[0] == path:  # sentinel for absolute paths
+            all_parts.insert(0, parts[0])
+            break
+        elif parts[1] == path: # sentinel for relative paths
+            all_parts.insert(0, parts[1])
+            break
+        else:
+            path = parts[0]
+            all_parts.insert(0, parts[1])
+    return all_parts
+
+
 def get_geometry(path):
     """
     function stolen and unammended
