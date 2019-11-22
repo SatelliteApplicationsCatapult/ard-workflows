638863075946b8e0bd3dcd904b8e612439fd85ce
Emily <emily.selwood@sa.catapult.org.uk>
2019-11-22 13:14:33 +0000

    file-name fiddling

--- utils/prepS1.py
+++ utils/prepS1.py
@@ -241,7 +241,7 @@ def yaml_prep_s1(scene_dir):
     prod_paths = glob.glob(scene_dir + '*.dim')
 
     logging.info(f"prod_path: {prod_paths}, scene_name: {scene_name}")
-    t0 = parse(str(datetime.strptime(os.path.dirname(prod_paths[0]).split("_")[-1], '%Y%m%dT%H%M%S')))
+    t0 = parse(str(datetime.strptime(scene_name.split("_")[-2], '%Y%m%dT%H%M%S')))
 
     # get polorisation from each image product (S1 band)
     # should be replaced with a more concise, generalisable parsing
